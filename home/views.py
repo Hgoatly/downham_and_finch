c72beb65d4f8c9b25c4fc8a66a98bc0d765273e9
Hgoatly <hgoatly@hotmail.com>
2021-04-08 21:50:46 +0000

    Add products views and templates.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
-    """ Return the index page """
+    """ Return the index page. This view
+    copied from the Boutique Ado project"""
 
     return render(request, 'home/index.html')
