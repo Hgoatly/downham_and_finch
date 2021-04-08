ff7efe2af689c43965fe3ab6eca55812546c8b70
Hgoatly <hgoatly@hotmail.com>
2021-04-08 17:07:34 +0000

    Customise admin.

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import Product, Product_type
 
-admin.site.register(Product)
-admin.site.register(Product_type)
 
+class ProductAdmin(admin.ModelAdmin):
+    """ This class has been copied
+    and adapted from the Boutique Ado Project """
+    list_display = (
+        'sku',
+        'name',
+        'display_name',
+        'colour',
+        'description',
+        'quantity_in_pack',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class Product_typeAdmin(admin.ModelAdmin):
+    """ This class has been copied and 
+    adapted from the Boutique Ado Project """
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Product_type, Product_typeAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Product_type(models.Model):
+    """ This model has been copied and
+    adapted from the Boutique Ado project """
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,7 +15,10 @@ class Product_type(models.Model):
 
 
 class Product(models.Model):
-    product_type = models.ForeignKey('Product_type', null=True, blank=True, on_delete=models.SET_NULL)
+    """ This model has been copied and
+    adapted from the Boutique Ado project """
+    product_type = models.ForeignKey(
+        'Product_type', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
@@ -21,8 +26,10 @@ class Product(models.Model):
     description = models.TextField()
     size = models.CharField(max_length=10, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity_in_pack = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
-    quantity_in_stock = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
+    quantity_in_pack = models.DecimalField(
+        max_digits=2, decimal_places=0, null=True, blank=True)
+    quantity_in_stock = models.DecimalField(
+        max_digits=2, decimal_places=0, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
