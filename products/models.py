9d4418cb81212ff1ab74aeec856e7e7f536e496a
Hgoatly <hgoatly@hotmail.com>
2021-04-08 14:48:33 +0000

    Load products data.

--- products/models.py
+++ products/models.py
@@ -16,9 +16,13 @@ class Product(models.Model):
     product_type = models.ForeignKey('Product_type', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    color = models.CharField(max_length=254, null=True, blank=True)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+    colour = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
+    size = models.CharField(max_length=10, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity_in_pack = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
+    quantity_in_stock = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
