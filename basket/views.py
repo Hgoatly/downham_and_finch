6e3d122a909054cbf4e5174d659a3d09ccd06192
Hgoatly <hgoatly@hotmail.com>
2021-04-11 11:29:58 +0000

    Add add to bag functionality.

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
@@ -6,3 +6,22 @@ def view_basket(request):
     This view copied from the Boutique Ado project"""
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ A view that adds a quantity of the selected product to the shopping basket. 
+    This view is copied from the Boutique Ado project."""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['bag'] = basket
+    print(request.session['bag'])
+    return redirect(redirect_url)
