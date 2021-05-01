121652d672bff0e6003e450c4b5f0904a582ef71
Hgoatly <hgoatly@hotmail.com>
2021-05-01 14:02:56 +0000

    Add 'event-listeners.js' file containing code to show/hide elements on 'profile.html'.

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,14 +1,27 @@
 // This file is copied and adapted from the Boutique Ado project.
 
-let countrySelected =$('#id_default_country, #id_country').val();
+let countrySelected =$('#id_default_country').val();
         if(!countrySelected) {
-            $('#id_default_country, #id_country').css('color', '#aab7c4');
+            $('#id_default_country').css('color', '#aab7c4');
         };
-        $('#id_default_country, #id_country').change(function() {
+        $('#id_default_country').change(function() {
             countrySelected = $(this).val()
             if(!countrySelected) {
                 $(this).css('color', '#aab7c4');
             } else {
                 $(this).css('color', '#000');
             }
-        })
+        });
+
+let deliveryCountrySelected =$('#id_country').val();
+        if(!deliveryCountrySelected) {
+            $('#id_country').css('color', '#aab7c4');
+        };
+        $('#id_country').change(function() {
+            deliveryCountrySelected = $(this).val()
+            if(!deliveryCountrySelected) {
+                $(this).css('color', '#aab7c4');
+            } else {
+                $(this).css('color', '#000');
+            }
+        });
