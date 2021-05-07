a476c18d0f64e5c22de10a0ef0109029b1eab52b
Hgoatly <hgoatly@hotmail.com>
2021-05-07 14:58:14 +0000

    Add 'delete' functionality.

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('fabric/<int:fabric_id>/', views.fabric_detail, name='fabric_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -82,13 +82,13 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
-        else: 
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
             messages.error(request, 'Product not added. Please check your form.')
     else:
-        form = ProductForm
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -118,6 +118,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
 
 
 def custom_products(request):
