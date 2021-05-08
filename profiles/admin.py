b1836dd3a687bc09dd75dc3e6ed5dfe0d1b70ab0
Hgoatly <hgoatly@hotmail.com>
2021-05-08 10:05:03 +0000

    Remove DeliveryAddress model, form and imports.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import DeliveryAddress, UserProfile
+from .models import UserProfile
 
-
-admin.site.register(DeliveryAddress),
 admin.site.register(UserProfile)
