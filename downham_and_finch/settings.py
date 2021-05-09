063d7c88a9cad080f3c649994e730ea34f95c161
Hgoatly <hgoatly@hotmail.com>
2021-05-09 18:35:42 +0000

    Remove secret key, and set debug to true in development only.

--- downham_and_finch/settings.py
+++ downham_and_finch/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(l$u=1mj==3vfpgji-qdz^5ix$$js-l1+)#3g%74i4&85ld3bi'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['downham-and-finch.herokuapp.com', 'localhost']
 
