6009856f03bd79e2fcc6f6a5836f6cbdf435b654
Hgoatly <hgoatly@hotmail.com>
2021-05-29 17:56:20 +0000

    Change navbar to gold text on black background.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -33,7 +33,7 @@ class StripeWH_Handler:
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
-            [cust_email, hgoatly@hotmail.com]
+            [cust_email]
         )
 
     def handle_event(self, event):
