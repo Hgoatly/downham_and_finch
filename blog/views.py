5cc13fa322f603e4ad5fb089421091f9f972cbb6
Hgoatly <hgoatly@hotmail.com>
2021-06-06 14:39:12 +0000

    Add address and phone number to footer. Complete blog view. Add blog to blog/admin.py.

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,10 @@ from .models import Blog
 def blog(request):
     """ A view to render blog posts """
 
+    blogs = Blog.objects.all()
+
+    context = {
+        'blogs': blogs,
+    }
     return render(request, 'blog/blog.html')
+
