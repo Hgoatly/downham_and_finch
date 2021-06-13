51bf0eab7963068b987f426e5c07a10f9ea3adb7
Hgoatly <hgoatly@hotmail.com>
2021-06-13 08:02:45 +0000

    Add datetime field to blog model, and order_by('-date_posted') to blog views.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=254)
     content = models.TextField(max_length=800)
     image = models.ImageField(null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,9 @@ from .models import Blog
 def blog(request):
     """ A view to render blog posts """
 
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.all().order_by('-date_posted')
 
     context = {
         'blogs': blogs,
     }
     return render(request, 'blog/blog.html', context)
-
