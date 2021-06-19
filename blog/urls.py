50f0eedacf492f5c4215b2121b3239cc93113c28
Hgoatly <hgoatly@hotmail.com>
2021-06-19 06:46:18 +0000

    Create edit_blog page and functionality.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ path('', views.blog, name='blog'),
 path('<int:blog_id>/', views.blog_detail, name='blog_detail'),
 path('delete/<int:blog_id>/', views.delete_blog, name='delete_blog'),
 path('add/', views.add_blog, name='add_blog'),
+path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -24,13 +24,13 @@ def blog_detail(request, blog_id):
     context = {
         'blog': blog,
     }
-    print(context)
 
     return render(request, 'blog/blog_detail.html', context)
 
 
 
 def add_blog(request):
+    """ View for adding blog posts """
     if not request.user.is_superuser:
         messages.error(request, "Sorry, you're not authorised do that.")
         return redirect(reverse('home'))
@@ -63,3 +63,32 @@ def delete_blog(request, blog_id):
     blog.delete()
     messages.success(request, 'Blog deleted!')
     return redirect(reverse('blog'))
+
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Edit an existing blog post"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, you are not authorised do that.')
+        return redirect(reverse('blog'))
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Blog successfully updated!')
+            return redirect(reverse('blog'))
+        else:
+            messages.error(
+                request, 'Unable to update blog. Please check your form.')
+    form = BlogForm(instance=blog)
+    messages.info(request, f'You are editing {blog.title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
+
