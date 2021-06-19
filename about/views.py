7774ab2e3276562de941a7565dd0cb7a3b78b328
Hgoatly <hgoatly@hotmail.com>
2021-06-19 14:34:41 +0000

    Create 'edit_blog.html' template.

--- about/views.py
+++ about/views.py
@@ -15,7 +15,29 @@ def about(request):
 
 
 
-def edit_about():
-    pass
+@login_required
+def edit_about(request):
+    """ Edit the 'about' content """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, you are not authorised do that.')
+        return redirect(reverse('about'))
+    about = get_object_or_404(About)
+    if request.method == 'POST':
+        form = AboutForm(request.POST, instance=about)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Content successfully updated!')
+            return redirect(reverse('about'))
+        else:
+            messages.error(
+                request, 'Unable to update content. Please check your form.')
+    form = AboutForm(instance=about)
+    messages.info(request, f'You are editing {about.title}')
 
+    template = 'about/edit_about.html'
+    context = {
+        'form': form,
+        'about': about,
+    }
 
+    return render(request, template, context)
