744f7b7eb67c56d16990ae934d7a56b6ead8c3de
Hgoatly <hgoatly@hotmail.com>
2021-06-27 10:59:42 +0000

    Add @xframe_options_exempt to 'index.html'.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
+@xframe_options_exempt
 def index(request):
     """ Return the index page. This view
     copied from the Boutique Ado project"""
